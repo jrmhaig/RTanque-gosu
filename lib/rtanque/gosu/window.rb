5ea87d9917ac4842f5f6f51feb83d7cdbc448853
Joe Haig <josephhaig@gmail.com>
2025-08-05 22:48:12 +0100

    Rubocop fixes for window.rb

--- lib/rtanque/gosu/window.rb
+++ lib/rtanque/gosu/window.rb
@@ -9,25 +9,13 @@ module RTanque
       FONT_NAME = ::Gosu.default_font_name
       SMALL_FONT_SIZE = Configuration.gui.fonts.small
 
-      attr_reader :gui_bots, :gui_shells, :gui_explosions
-
       def initialize(match)
         @match = match
         @arena = match.arena
         match.bots.each { |bot| bot.gui_window = self }
-        @gui_bots = DrawGroup.new(match.bots) do |bot|
-          RTanque::Gosu::Bot.new(self, bot)
-        end
-        @gui_shells = DrawGroup.new(match.shells) do |shell|
-          RTanque::Gosu::Shell.new(self, shell)
-        end
-        @gui_explosions = DrawGroup.new(match.explosions) do |explosion|
-          RTanque::Gosu::Explosion.new(self, explosion)
-        end
         # Fullscreen: https://github.com/jlnr/gosu/issues/159#issuecomment-12473172
         super(@arena.width, @arena.height, false, UPDATE_INTERVAL)
         self.caption = self.class.name.split('::').first
-        @background = ::Gosu::Image.new(self, Gosu.resource_path('images/grass.png'))
 
         @draw_procs = []
       end
@@ -38,7 +26,7 @@ module RTanque
 
       def draw
         close if button_down?(::Gosu::Button::KbEscape)
-        @background.draw(0, 0, ZOrder::BACKGROUND)
+        background.draw(0, 0, ZOrder::BACKGROUND)
         if @match.finished?
           close if Configuration.quit_when_finished
           gui_bots.each { |bot| bot.grow(2) }
@@ -58,8 +46,32 @@ module RTanque
         @draw_procs.clear
       end
 
-      def add_draw_proc(&b)
-        @draw_procs << b
+      def add_draw_proc(&block)
+        @draw_procs << block
+      end
+
+      def gui_bots
+        @gui_bots ||= DrawGroup.new(@match.bots) do |bot|
+          RTanque::Gosu::Bot.new(self, bot)
+        end
+      end
+
+      def gui_shells
+        @gui_shells ||= DrawGroup.new(@match.shells) do |shell|
+          RTanque::Gosu::Shell.new(self, shell)
+        end
+      end
+
+      def gui_explosions
+        @gui_explosions ||= DrawGroup.new(@match.explosions) do |explosion|
+          RTanque::Gosu::Explosion.new(self, explosion)
+        end
+      end
+
+      private
+
+      def background
+        @background ||= ::Gosu::Image.new(self, Gosu.resource_path('images/grass.png'))
       end
     end
   end
