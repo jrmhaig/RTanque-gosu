4f4ff21b28dcedcd0a78f83d0401224d520dfb43
Joe Haig <josephhaig@gmail.com>
2025-08-11 19:56:27 +0100

    Use image for health bar
    The previous method of creating the health bar from binary blobs caused performance issues.

--- lib/rtanque/gosu/bot/health_color_calculator.rb
+++ lib/rtanque/gosu/bot/health_color_calculator.rb
@@ -4,10 +4,11 @@ module RTanque
   class Gosu
     class Bot
       class HealthColorCalculator
-        # different health-clors as RGB values
-        FULL_HEALTH_COLOR   = [74, 190, 74, 255].freeze
-        MEDIUM_HEALTH_COLOR = [255, 190,   0, 255].freeze
-        LOW_HEALTH_COLOR    = [220,   0,   0, 255].freeze
+        # Different health-colors as RGB values
+        #                      Blue, Green,Red,  Alpha
+        FULL_HEALTH_COLOR   = [0x4A, 0xBE, 0x4A, 0xFF].freeze
+        MEDIUM_HEALTH_COLOR = [0x00, 0xBE, 0xFF, 0xFF].freeze
+        LOW_HEALTH_COLOR    = [0x00, 0x00, 0xDC, 0xFF].freeze
 
         attr_reader :health
 
@@ -15,7 +16,7 @@ module RTanque
           @health = health
         end
 
-        def color_as_rgba
+        def color
           if health > 50
             percentage = ((100 - health) / 50)
             color_between(FULL_HEALTH_COLOR, MEDIUM_HEALTH_COLOR, percentage)
@@ -28,11 +29,11 @@ module RTanque
         private
 
         def color_between(color_a, color_b, percentage)
-          4.times.map { |i| graduated(color_a[i], color_b[i], percentage) }.join
+          4.times.map { |i| graduated(color_a[i], color_b[i], percentage) << (8 * i) }.sum
         end
 
         def graduated(min, max, percentage)
-          (((max - min) * percentage) + min).round.chr
+          (((max - min) * percentage) + min).round
         end
       end
     end
